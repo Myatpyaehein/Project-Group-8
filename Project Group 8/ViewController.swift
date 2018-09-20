893c8f0381e38faad52f1ce2383083f3db3d966a
Myat Pyae Hein <myatpyaehein@facebook.com>
2018-09-19 17:01:24 -0700

    Initial Commit

---
+++ Project Group 8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project Group 8
+//
+//  Created by Group 8 on 9/19/18.
+//  Copyright © 2018 Myat Hein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
