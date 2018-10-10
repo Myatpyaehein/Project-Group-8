09da354e24e1171cbc762429c417b4466e2b464b
pooldiver69 <38307992+pooldiver69@users.noreply.github.com>
2018-10-09 19:10:12 -0700

    started the question table file

---
+++ Project Group 8/QuestionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TableViewController.swift
+//  Project Group 8
+//
+//  Created by Feiyu Cai on 10/9/18.
+//  Copyright © 2018 Myat Hein. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class QuestionViewController: UITableViewController {
+    
+}
