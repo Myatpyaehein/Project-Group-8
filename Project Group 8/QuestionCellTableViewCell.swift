9b20655cacd54f46838012092de15516f0618207
pooldiver69 <38307992+pooldiver69@users.noreply.github.com>
2018-11-27 18:34:38 -0800

    update

---
+++ Project Group 8/QuestionCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  QuestionCellTableViewCell.swift
+//  Project Group 8
+//
+//  Created by Feiyu Cai on 11/27/18.
+//  Copyright © 2018 Myat Hein. All rights reserved.
+//
+
+import UIKit
+
+class QuestionCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Project Group 8/QuestionViewController.swift
+++ Project Group 8/QuestionViewController.swift
@@ -9,6 +9,19 @@
 import Foundation
 import UIKit
 
+struct Question {
+    var questionString: String?
+    var answer: [String]?
+}
 class QuestionViewController: UITableViewController {
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "QuestionCell", for: <#T##IndexPath#>)
+        cell.textLabel?.text = 
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        <#code#>
+    }
     
 }
